f8ae1b14653633f8e795f2fcb38e142cbb9d78f8
Pariterre <pariterre@hotmail.com>
2023-05-29 19:24:32 -0400

    Finish first version of configuration screen

--- lib/screens/configuration_room.dart
+++ lib/screens/configuration_room.dart
@@ -19,6 +19,7 @@ class _ConfigurationRoomState extends State<ConfigurationRoom> {
   final _nbMaxPlayersController = TextEditingController();
   final _nbRowsController = TextEditingController();
   final _nbColsController = TextEditingController();
+  final _nbBombsController = TextEditingController();
 
   @override
   void didChangeDependencies() {
@@ -33,33 +34,42 @@ class _ConfigurationRoomState extends State<ConfigurationRoom> {
         _mainInterface.gameManager.maxPlayers.toString();
     _nbRowsController.text = _mainInterface.gameManager.nbRows.toString();
     _nbColsController.text = _mainInterface.gameManager.nbCols.toString();
+    _nbBombsController.text = _mainInterface.gameManager.nbBombs.toString();
   }
 
   void _goToIdleRoom() {
+    _mainInterface.gameManager.setGameParameters(
+      maximumPlayers: int.tryParse(_nbMaxPlayersController.text),
+      nbRows: int.tryParse(_nbRowsController.text),
+      nbCols: int.tryParse(_nbColsController.text),
+      nbBombs: int.tryParse(_nbBombsController.text),
+    );
+
     Navigator.of(context)
         .pushReplacementNamed(IdleRoom.route, arguments: _mainInterface);
   }
 
   Widget _buildMaxPlayersForm() {
-    final windowHeight = MediaQuery.of(context).size.height;
+    final textSize = ThemeSize.text(context);
 
-    return Container(
-      width: windowHeight * 0.5,
-      padding: const EdgeInsets.only(top: 10),
-      decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(5))),
-      child: TextField(
-        controller: _nbMaxPlayersController,
-        onChanged: (value) => setState(() {
-          if (int.tryParse(value) == null) {
-            _nbMaxPlayersController.text = "";
-          }
-        }),
-        decoration:
-            const InputDecoration(labelText: 'Nombre de joueurs maximum'),
-        style: const TextStyle(color: Colors.black),
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text('Nombre de joueurs maximum :',
+            style: TextStyle(color: Colors.white, fontSize: textSize)),
+        SizedBox(
+          width: 50,
+          child: TextField(
+            controller: _nbMaxPlayersController,
+            onChanged: (value) => setState(() {
+              if (int.tryParse(value) == null) {
+                _nbMaxPlayersController.text = "";
+              }
+            }),
+            style: const TextStyle(color: Colors.black),
+          ),
+        ),
+      ],
     );
   }
 
@@ -67,30 +77,59 @@ class _ConfigurationRoomState extends State<ConfigurationRoom> {
     final textSize = ThemeSize.text(context);
 
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Text('Dimensions :',
             style: TextStyle(color: Colors.white, fontSize: textSize)),
         const SizedBox(width: 10),
-        SizedBox(
-          width: 50,
-          child: TextField(
-            controller: _nbRowsController,
-            onChanged: (value) => setState(() {
-              if (int.tryParse(value) == null) {
-                _nbRowsController.text = "";
-              }
-            }),
-            style: const TextStyle(color: Colors.black),
-          ),
+        Row(
+          children: [
+            SizedBox(
+              width: 50,
+              child: TextField(
+                controller: _nbRowsController,
+                onChanged: (value) => setState(() {
+                  if (int.tryParse(value) == null) {
+                    _nbRowsController.text = "";
+                  }
+                }),
+                style: const TextStyle(color: Colors.black),
+              ),
+            ),
+            const SizedBox(width: 10),
+            SizedBox(
+              width: 50,
+              child: TextField(
+                controller: _nbColsController,
+                onChanged: (value) => setState(() {
+                  if (int.tryParse(value) == null) {
+                    _nbColsController.text = "";
+                  }
+                }),
+                style: const TextStyle(color: Colors.black),
+              ),
+            ),
+          ],
         ),
-        const SizedBox(width: 10),
+      ],
+    );
+  }
+
+  Widget _buildBombsForm() {
+    final textSize = ThemeSize.text(context);
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text('Nombre de bombes :',
+            style: TextStyle(color: Colors.white, fontSize: textSize)),
         SizedBox(
           width: 50,
           child: TextField(
-            controller: _nbColsController,
+            controller: _nbBombsController,
             onChanged: (value) => setState(() {
               if (int.tryParse(value) == null) {
-                _nbColsController.text = "";
+                _nbBombsController.text = "";
               }
             }),
             style: const TextStyle(color: Colors.black),
@@ -124,14 +163,15 @@ class _ConfigurationRoomState extends State<ConfigurationRoom> {
       ),
       child: Center(
         child: Container(
-            width: windowHeight * 0.7,
             height: windowHeight * 0.5,
+            width: windowHeight * 0.5,
             decoration: const BoxDecoration(color: ThemeColor.main),
             child: Padding(
               padding: EdgeInsets.only(
                   left: smallPadding,
                   top: smallPadding,
-                  bottom: smallPadding * 1.5),
+                  bottom: smallPadding * 1.5,
+                  right: smallPadding),
               child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -147,19 +187,10 @@ class _ConfigurationRoomState extends State<ConfigurationRoom> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           _buildMaxPlayersForm(),
-                          SizedBox(height: 2 * interlinePadding),
+                          SizedBox(height: 4 * interlinePadding),
                           _buildDimensionsForm(),
-                          // Text(
-                          //   'Dimension du terrain : ${_mainInterface.gameManager.nbRows}x${_mainInterface.gameManager.nbCols}',
-                          //   style: TextStyle(
-                          //       color: Colors.white, fontSize: textSize),
-                          // ),
-                          // SizedBox(height: interlinePadding),
-                          // Text(
-                          //   'Nombre d\'essais : ${_mainInterface.gameManager.nbBombs}',
-                          //   style: TextStyle(
-                          //       color: Colors.white, fontSize: textSize),
-                          // ),
+                          SizedBox(height: 4 * interlinePadding),
+                          _buildBombsForm(),
                         ],
                       ),
                     ),
@@ -171,12 +202,10 @@ class _ConfigurationRoomState extends State<ConfigurationRoom> {
 
   @override
   Widget build(BuildContext context) {
-    final windowWidth = MediaQuery.of(context).size.width;
     final windowHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
       body: Container(
-        width: windowWidth,
         height: windowHeight,
         decoration: const BoxDecoration(color: ThemeColor.greenScreen),
         child: Stack(
