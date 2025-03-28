7aec93b3b410516e4e6b16c8dd4a197b6d790821
Pariterre <pariterre@hotmail.com>
2025-03-28 19:57:59 -0400

    Merged to Train de mots

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,7 @@
 import 'package:common/managers/theme_manager.dart';
 import 'package:flutter/material.dart';
-import 'package:twitch_treasure_seeker/managers/treasure_seeker_game_manager.dart';
-import 'package:twitch_treasure_seeker/managers/twitch_manager.dart';
-import 'package:twitch_treasure_seeker/screens/game_screen.dart';
-
-// TODO: Add sound effects
-// TODO: Add frontend
-// TODO: Add backend
 
 void main() async {
   await ThemeManager.factory();
-  TwitchManager.instance.initialize(useMock: false);
-
-  GameManager.instance; // Force the initialization of the game manager
-
-  runApp(MaterialApp(
-    initialRoute: GameScreen.route,
-    routes: {
-      GameScreen.route: (ctx) => const GameScreen(),
-    },
-  ));
+  runApp(MaterialApp());
 }
